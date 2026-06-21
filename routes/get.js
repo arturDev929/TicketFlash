750db0916e341da747d0584716958e7f7e2b42bc
Goven Leya <leyagoven@gmail.com>
2026-06-21 13:16:38 +0100

    feat: add the news modification that Artur did

--- routes/get.js
+++ routes/get.js
@@ -341,6 +341,7 @@ router.get('/movies/:id_filme', (req, res) => {
 
 router.get('/sessoes', (req, res) => {    
     const query = `SELECT 
+                        f.id_filme,
                         f.titulo,
                         f.duracao_minutos,
                         f.ano_lancamento,
